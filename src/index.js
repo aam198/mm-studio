9b26bb83d47ac08c624aed67f0ed42462cce833c
aam198 <aam198@psu.edu>
2023-03-29 14:31:22 -0400

    Testimonal style build & function build start

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log(123);
+// Testimonial Section
+const testimonialContainer = document.querySelector('.testimonial-container');
+const testimonial = document.querySelector('.testimonial');
+const userImage = document.querySelector('.user-image');
+const username = document.querySelector('.username');
+const role = document.querySelector('.role');
