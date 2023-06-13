50cf8277b3484efeb23ca3344acdda45f7fc8ac0
aam198 <aam198@psu.edu>
2023-06-13 14:49:17 -0400

    Main page: carousel setTimeout add for fade-in of each image

--- src/carousel.js
+++ src/carousel.js
@@ -21,16 +21,23 @@ function showSlides(n) {
   if (n < 1) 
     {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
+    slides[i].style.opacity="0";
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
+  // slides[slideIndex-1].style.opacity=1;
   dots[slideIndex-1].className += " active";
+
+  // Fade-in effect
+  setTimeout(function() {
+    slides[slideIndex -1].style.opacity="1";
+  }, 100);
 } 
 
 // setInterval added for auto 
 setInterval(function() {
   plusSlides(1);
-}, 3000);
+}, 4000);
