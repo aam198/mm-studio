b7a11aadba19d489385cebcc90c335a2db0a380c
aam198 <aam198@psu.edu>
2023-06-13 14:42:55 -0400

    Main page: carousel setInterval function added

--- src/carousel.js
+++ src/carousel.js
@@ -1,6 +1,7 @@
 
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
+
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -11,66 +12,25 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("carousel-img");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
+  let i;
+  const slides = document.getElementsByClassName("carousel-img");
+  const dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) 
+    {slideIndex = 1}
+  if (n < 1) 
+    {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 } 
 
-
-
-// const imgs = document.getElementById('imgs');
-// const leftBtn= document.getElementById('left');
-// const rightBtn= document.getElementById('right');
-// const img = document.querySelectorAll('#imgs img');
-
-
-// let index = 0;
-
-// // Every 2 seconds, function run will 'run'
-// let interval = setInterval(run, 2000)
-
-
-// function run() {
-//   index ++;
-//   changeImage()
-// }
-
-// // Updating the translateX property in the CSS
-// function changeImage(){
-//   // To find if we are at the end of the node list
-//   if(index > img.length - 1 ){
-//     index = 0;
-//   }
-//   else if (index < 0 ){
-//     index = img.length - 1;
-//   }
-
-//   imgs.style.transform = `translateX(${-index * 500}px)`;
-// }
-
-// function resetInterval() {
-//   clearInterval(interval)
-//   interval = setInterval(run, 2000);
-// }
-
-// rightBtn.addEventListener('click', () => {
-//   index ++;
-//   changeImage();
-//   resetInterval();
-// })
-
-// leftBtn.addEventListener('click', () => {
-//   index --;
-//   changeImage();
-//   resetInterval();
-// })
+// setInterval added for auto 
+setInterval(function() {
+  plusSlides(1);
+}, 3000);
