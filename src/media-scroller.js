b8a8b44e9d2bf424d94f276b59c26753a02a2632
aam198 <aam198@psu.edu>
2024-11-20 10:02:27 -0500

    bug fix for media scroller

--- src/media-scroller.js
+++ src/media-scroller.js
@@ -10,19 +10,29 @@ const scrollRightButton = document.getElementById('scroll_right');
 
 // Click Event for scrolling left
 scrollLeftButton.addEventListener('click', () => {
-  console.log("Left clicked");
+  console.log("Left clicked")
 
   scrollContainer.scrollBy({
     left: -scrollAmount,
     behavior: 'smooth' 
-  });
+  })
 });
 
-scrollRightButton.addEventListener('click', () => { 
-  console.log("Right clicked");
+scrollRightButton.addEventListener('click', function() {
+  console.log('right clicked')
 
   scrollContainer.scrollBy({
-    right: scrollAmount,
+    left: scrollAmount,
     behavior: 'smooth'
-  });
+  })
 });
+
+
+
+// document.getElementById('scroll_right').addEventListener('click', function() {
+//   const scroller = document.querySelector('.media-scroller');
+//   scroller.scrollBy({
+//       left: 300, // Change this value to adjust the scroll amount
+//       behavior: 'smooth' // Adds smooth scrolling
+//   });
+// });
