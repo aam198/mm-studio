6ac764b1ccb363b828ccc277c46f373256331898
aam198 <aam198@psu.edu>
2025-05-06 11:02:10 -0400

    Link update for styles.css

--- src/testimonial.js
+++ src/testimonial.js
@@ -1,11 +1,9 @@
-
 // Testimonial Section
 const testimonialContainer = document.querySelector('.testimonial-container');
 const testimonial = document.querySelector('.testimonial');
 const userImage = document.querySelector('.user-image');
 const username = document.querySelector('.username');
 const role = document.querySelector('.role');
-
 
 const testimonials = [ 
   {
